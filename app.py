c90cd066387f9aeda7ae90fac8a7a37bd7dc86eb
Lucas Azevedo <>
2023-03-23 19:20:19 -0300

    initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__, template_folder="./views/", static_folder="./static/")
+
+
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
