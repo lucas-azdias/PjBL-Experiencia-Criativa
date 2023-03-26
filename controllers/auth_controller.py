4340c3fcdbaaa1aa2e3374ae2c6e191d7ec58198
Lucas Azevedo <>
2023-03-26 16:48:26 -0300

    login update

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -8,9 +8,15 @@ auth = Blueprint("auth", __name__, template_folder="./views/", static_folder="./
 # DADOS PUXADOS DO BANCO DE DADOS
 users = [("admin", "12345678", True), ("roberto", "gomes", False), ("bafome", "jesus", True)] # (username, password, is_admin)
 
+isLogged = False
+isAdmin = False
+
 
 @auth.route("/")
 def auth_index():
+    global isLogged, isAdmin
+    isLogged = False
+    isAdmin = False
     return render_template("/auth/auth_index.html")
 
 
@@ -19,35 +25,59 @@ def auth_login():
     username = request.form.get("username")
     password = request.form.get("password")
 
-    if (username, password) in users:
-        return url_for("auth.auth_users_manager")
+    ifAdmin = (username, password, True) in users
+    ifComum = (username, password, False) in users
+
+    global isLogged, isAdmin
+    if ifAdmin or ifComum:
+        isLogged = True
+        if ifAdmin:
+            isAdmin = True
+            return url_for("auth.auth_users_manager")
+        else:
+            isAdmin = False
+            return url_for("index")
     else:
+        isLogged = False
+        isAdmin = False
         return url_for("auth.auth_index")
 
 
 @auth.route("/users_manager")
 def auth_users_manager():
-    # if isLogged:
-    usernames = [user[0] for user in users]
-    is_admin = [user[2] for user in users]
-    return render_template("/auth/auth_users_manager.html", usernames=usernames, is_admin=is_admin)
-    # else:
-    #     return redirect(url_for("auth.auth_index"))
+    if isLogged and isAdmin:
+        usernames = [user[0] for user in users]
+        is_admin = [user[2] for user in users]
+        return render_template("/auth/auth_users_manager.html", usernames=usernames, is_admin=is_admin)
+    elif isLogged and not isAdmin:
+        return redirect(url_for("index"))
+    else:
+        return redirect(url_for("auth.auth_index"))
 
 
 @auth.route("/get_usernames", methods=["POST"])
 def auth_get_usernames():
-    usernames = [user[0] for user in users]
-    return dumps(usernames)
+    if isLogged and isAdmin:
+        usernames = [user[0] for user in users]
+        return dumps(usernames)
+    elif isLogged and not isAdmin:
+        return redirect(url_for("index"))
+    else:
+        return redirect(url_for("auth.auth_index"))
 
 
 @auth.route("/add_user", methods=["POST"])
 def auth_add_user():
-    username = request.form.get("username")
-    password = request.form.get("password")
-    is_admin = int(request.form.get("is_admin"))
+    if isLogged and isAdmin:
+        username = request.form.get("username")
+        password = request.form.get("password")
+        is_admin = int(request.form.get("is_admin"))
 
-    user = (username, password, True if is_admin == 1 else False)
-    if not user in users:
-        users.append(user)
-    return url_for("auth.auth_users_manager")
+        user = (username, password, True if is_admin == 1 else False)
+        if not user in users:
+            users.append(user)
+        return url_for("auth.auth_users_manager")
+    elif isLogged and not isAdmin:
+        return redirect(url_for("index"))
+    else:
+        return redirect(url_for("auth.auth_index"))
