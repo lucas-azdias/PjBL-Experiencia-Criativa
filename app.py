de4af960a9fc778fc0769063a8e4ac73448e9ead
Lucas Azevedo <>
2023-03-26 12:25:07 -0300

    update

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template
 
+from controllers.auth_controller import auth
+
 
 app = Flask(__name__, template_folder="./views/", static_folder="./static/")
+
+app.register_blueprint(auth, url_prefix="/auth")
 
 
 @app.route('/index')
