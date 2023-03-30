82a113a6afa826b351e6b4890ef998e89c8a4da0
Kozak <107823955+SedentaryDust@users.noreply.github.com>
2023-03-30 11:20:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,12 @@ app = Flask(__name__, template_folder="./views/", static_folder="./static/")
 
 app.register_blueprint(auth, url_prefix="/auth")
 app.register_blueprint(data, url_prefix="/data")
+tags = ["Autenticacao usuario" , "Formulario de resgistro de dados" , "Grafico IOT"]
+hrefs = ["/auth" , "/data" , "/grafico"]
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html" , ,hrefs  = hrefs, tags = tags)
 
 @app.route('/grafico')
 def grafico():
