033d07528e1d93d233b7fba25d6c3640f9d8fac2
Lucas Azevedo <>
2023-04-20 12:02:13 -0300

    update

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from controllers.app_controller import createApp
+from utils.create_db import create_db
 
 
 if __name__ == "__main__":
     app = createApp()
+    create_db(app)
     app.run(debug=True)
