4c76cb585f8f19c394668de60a2e6e06075c202e
Lucas Azevedo <>
2023-04-20 10:26:20 -0300

    update

--- controllers/plants_controller.py
+++ controllers/plants_controller.py
@@ -6,6 +6,7 @@ plants = Blueprint("plants", __name__, template_folder="./views/", static_folder
 
 saved_plants = []
 
+
 @plants.route('/')
 def plants_index():
     global saved_plants
@@ -17,16 +18,15 @@ def plants_register_plant():
     global saved_plants
     return render_template('plants/plants_register_plant.html', saved_plants=saved_plants)
 
+
 @plants.route('/save_plant', methods=['POST'])
 def plants_save_plant():
     plant_data = {
     'nome_planta': request.form.get("nome_planta"),
     'sensor': request.form.get("sensor"),
     'umidade': request.form.get("umidade")
-}
+    }
     global saved_plants
     saved_plants.append(plant_data)
     #saved_plants.append("Planta " + nome_planta + ", Sensor " + sensor + ", Umidade mínima " + umidade)
     return redirect(url_for("plants.plants_index"))
-
-
